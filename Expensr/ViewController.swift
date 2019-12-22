4ecdfb19fede24d16fa801ec00b5b116ebfe3ba1
Brendyn Dabrowski <brendyn.dabrowski@cerner.com>
2019-12-22 15:03:24 -0600

    Initial Commit

---
+++ Expensr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expensr
+//
+//  Created by Dabrowski,Brendyn on 12/22/19.
+//  Copyright © 2019 BDCreative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
